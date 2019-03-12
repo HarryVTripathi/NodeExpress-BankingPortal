f1416fa5fa87b53f42875e68c4295c3caee5bf2c
HarryVTripathi <harshvardhan.tripathi2015@vit.ac.in>
2019-03-12 15:36:58 +0530

    updated index

--- src/app.js
+++ src/app.js
@@ -25,9 +25,11 @@ const users = JSON.parse(userData);
 //  render the view that we defined in index.ejs file
 app.get('/', (req, res) => res.render('index', {title: 'Account Summary', accounts: accounts}));
 
-app.get('/savings', (req, res) => res.render('account', {account: accounts.savings}))
-app.get('/checking', (req, res) => res.render('account', {account: accounts.checking}))
-app.get('/credit', (req, res) => res.render('account', {account: accounts.credit}))
+app.get('/savings', (req, res) => res.render('account', {account: accounts.savings}));
+app.get('/checking', (req, res) => res.render('account', {account: accounts.checking}));
+app.get('/credit', (req, res) => res.render('account', {account: accounts.credit}));
+
+app.get('/profile', (req, res) => res.render('profile', {user: users[0]}));
 
 app.listen(3000, () => {
     console.log('PS project running on port 3000!');
